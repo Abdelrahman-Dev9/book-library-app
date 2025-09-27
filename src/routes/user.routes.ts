981cac5b7b5b708ace4d4e4f27cba9ee450c9cff
Abdelrahman-Dev9 <abdoelnagar198@gmail.com>
2025-09-28 00:34:04 +0300

    Create delete  Method to delete  Book

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { createBook } from "../controllers/CreateBook.controllers";
 import { getAllBooks } from "../controllers/GetAllBooks.controllers.";
 import { getOneBook } from "../controllers/CreateBookById.controllers";
+import { deleteBook } from "../controllers/DeleteBook.controllers";
 
 const router = Router();
 
 router.post("/createBook", createBook);
 router.get("/getAllBooks", getAllBooks);
 router.get("/getOneBook/:id", getOneBook);
+router.delete("/deleteBook/:id", deleteBook);
 
 export default router;
